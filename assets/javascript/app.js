8271c43ee5ee1e99a7b82b99d0e387663504c358
Matt Davis <matt_davis@me.com>
2016-09-11 23:45:15 -0500

    Code cleanup, added more stats, stat reset button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,49 @@
 // Main game object
 let game = {
-	// Properties
-	currentLetter: 0,
-	ready: false,
-	score: 0,
-
-	// Word object arrays
+	// Game state vars
 	activeWords: [],
 	matchingWords: [],
+	currentLetter: 0,
+	ready: false,
 
-	// Timing vars
+	// Stats vars
+	score: 0,
+	currentStreak: 0,
+	longestStreak: 0,
+	hits: 0,
+	misses: 0,
 	startTime: Date.now(),
+	timeOffset: 0,
 
 	// Methods
 	init: function() {
 		game.newWord(0);
 		game.newWord(1);
 		game.newWord(2);
+		game.updateStats();
+		setTimeout(function(){ game.timeOffset /= 3; }, 200);
 	},
 
 	newWord: function(index) {
+		// Do not allow keyboard input (until ajax request completes)
+		game.ready = false;
+
+		// If word is replacing an existing word (a word was completed), increment score, update stats, reset game vars
+		if (game.activeWords[index]) {
+			game.score += 10;
+			game.hits += game.currentLetter;
+			game.currentStreak++
+			if (game.currentStreak > game.longestStreak) { game.longestStreak = game.currentStreak; }
+
+			game.currentLetter = 0;
+			game.matchingWords = [];
+			game.updateStats();
+		}
+
+		// Log start time of request
+		const startTime = Date.now();
+
+		// Request new word
 		$.ajax({
             url: "http://www.setgetgo.com/randomword/get.php",
             method: "GET",
@@ -27,117 +51,123 @@ let game = {
             	len: 6
             }
         }).done(function(response){
-        	let word = {
+        	// Add request time to timeOffset
+			game.timeOffset += Date.now() - startTime;
+
+        	const word = {
 				str: response,
-				arr: [],
-				html: "",
 				number: index
 			};
 
 			// Create and output series of spans for each character in the word
-			word.html += "<h2>";
+			let html = "<h2>";
 			for (let i = 0; i < word.str.length; i++) {
-				word.arr.push(word.str.charAt(i).toLowerCase());
-				word.html += "<span id='word_" + index + "_letter_" + i + "'>" + word.arr[i] + "</span>";
+				html += "<span id='word_" + index + "_letter_" + i + "'>" + word.str[i] + "</span>";
 			}
-			word.html += "</h2>";
-			$("#out_" + index).html(word.html);
+			html += "</h2>";
+			$("#out_" + index).html(html);
 
-			// Add word to activeWords and a copy to checkWords
+			// Add word to activeWords
 			game.activeWords[index] = word;
-
-			// Update all displayed stats
-			game.updateStats();
-
-			// Allow game to proceed
+			
+			// Allow keyboard input
 			game.ready = true;
         });
 	},
 
 	resetWordHtml: function(word) {
-		// Reset all letters to initial state
-		for (let i = 0; i < word.str.length; i++) {
+		for (let i = 0; i < game.currentLetter; i++) {
 			$("#word_" + word.number + "_letter_" + i).css("color", "black");
 		}
 	},
 
-	wordMissAnimation: function(word) {
-		$("#out_" + word.number).css("left", -3);
-		setTimeout(function(){ $("#out_" + word.number).css("left", 6); }, 30);
-		setTimeout(function(){ $("#out_" + word.number).css("left", -6); }, 70);
-		setTimeout(function(){ $("#out_" + word.number).css("left", 3); }, 100);
-		setTimeout(function(){ $("#out_" + word.number).css("left", 0); }, 110);
+	wordMiss: function(wordList) {
+		const animationTime = 110;
+		game.timeOffset += animationTime;
+
+		game.ready = false;
+		game.hits += game.currentLetter;
+		game.misses++;
+		game.currentStreak = 0;
+
+		
+		for (const word of wordList) {
+			$("#out_" + word.number).css("left", -3);
+			setTimeout(function(){ $("#out_" + word.number).css("left", 6); }, 30);
+			setTimeout(function(){ $("#out_" + word.number).css("left", -6); }, 70);
+			setTimeout(function(){ $("#out_" + word.number).css("left", 3); }, 100);
+			setTimeout(function(){ $("#out_" + word.number).css("left", 0); game.ready = true; }, animationTime);
+		}
+
+		game.currentLetter = 0;
+		game.updateStats();
 	},
 
 	updateStats: function() {
-		const wpm = (game.score / 10) / ((Date.now() - game.startTime) / 1000 / 60);
-		$("#wpm").html(wpm.toFixed(1));
-		$("#score").html("<h3>" + game.score + "</h3>");
+		const wpm = (game.score / 10) / ((Date.now() - game.startTime - game.timeOffset) / 1000 / 60) || 0;
+		const acc = 100 * game.hits / (game.hits + game.misses) || 0;
+
+		$("#score").html("<h3>Score: " + game.score + "</h3>");
+		$("#wpm").html("WPM: " + wpm.toFixed(1));
+		$("#acc").html("Accuracy: " + game.hits + " / " + (game.hits + game.misses) + " ( " + acc.toFixed(1) + "% )");
+		$("#streak").html("Perfect streak: " + game.currentStreak + "<br/>Longest streak: " + game.longestStreak);
 	}
 };
 
-// On load
+// On load block
 $(function() {
-	$(document).on("keypress", function(event) {
-		// If game is ready for input and keystroke is a letter
-		if (game.ready && (event.which >= 65) && (event.which <= 122)) {
 
-			const keystroke = String.fromCharCode(event.which).toLowerCase();
-			let match = false;
-			let missedWords = [];
+	// Main input processing function (on any keypress)
+	$(document).on("keypress", function(e) {
+		// If game is ready for input and keystroke is a letter
+		if (game.ready && (e.which >= 65) && (e.which <= 122)) {
+			let missedWords = [];  // Keeps an array of all remaining possible matches that miss this cycle
 
 			// If checkWords is empty, copy all words from activeWords
 			if (game.matchingWords.length == 0) { game.matchingWords = game.activeWords.slice(); }
 
 			// Check all remaining words in checkingWords for matches
 			for (let i = 0; i < game.matchingWords.length; i++) {
-				let word = game.matchingWords[i];
+				const word = game.matchingWords[i];
 
-				// If the latest keystroke matches the current letter in the checked word
-				if (word.arr[game.currentLetter] == keystroke) {
-
-					// Update red letters on screen, advance game letter, indicate a match (non-miss)
+				// If the latest keystroke matches the current letter, make it red
+				if (word.str[game.currentLetter] == String.fromCharCode(e.which)) {
 					$("#word_" + word.number + "_letter_" + game.currentLetter).css("color", "red");
-					match = true;
-
-					// If word is complete, replace with new word, increment score
-					if (game.currentLetter == word.str.length - 1) {
-						game.ready = false;
-						game.score += 10;
-						game.currentLetter = 0;
-						game.matchingWords = [];
-						game.newWord(word.number);
-						game.updateStats();
-					}
-				
 				} else {
-					// Reset and remove non-matching words
-					game.resetWordHtml(word);
+					// Add word to missedWords, reset letter colors, remove word from matchingWords, decrement loop counter
 					missedWords.push(word);
+					game.resetWordHtml(word);
 					game.matchingWords.splice(i, 1);
 					i--;
 				}
 			}
 
-			// If new word has been issued, do not execute
-			if (game.ready) {
+			// If a match is found, increment currentLetter
+			if (game.matchingWords.length > 0) {
+				game.currentLetter++;
 
-				// If a match is found, increment currentLetter
-				if (match) { game.currentLetter++; }
-				else {
-					// If no matches are found, reset currentLetter to 0, animate all relevant words
-					game.currentLetter = 0;
-					for (const word of missedWords) {
-						game.wordMissAnimation(word);
-					}
-					game.updateStats();
+				// If word is complete, replace with new word
+				if (game.currentLetter == game.matchingWords[0].str.length) {
+					game.newWord(game.matchingWords[0].number);
 				}
 			}
-
-			console.log(game.currentLetter);
-			console.log(game.matchingWords);
+			else {
+				game.wordMiss(missedWords);
+			}
 		}
 	});
+
+	// Clear stats button - resets all stats to zero
+	$("#clearStats").on("click", function(e){
+		game.score = 0;
+		game.currentStreak = 0;
+		game. longestStreak = 0;
+		game.hits = 0;
+		game.misses = 0;
+		game.startTime = Date.now();
+		game.timeOffset = 0;
+		game.updateStats();
+	})
 
 	game.init();
 });
